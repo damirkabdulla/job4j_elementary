241782ba490c1472be2f5578129d4efea8000b66
admin <the13.demo@gmail.com>
2022-09-16 16:30:55 +0700

    Разбор первой программы. [#159033 #221767]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Petr Arsentev");
+        System.out.println("10.04.1997");
+    }
+}
